3384c34315e2eff4ddae3d1d4f7f0ac1ef288b70
Diana Daia <diana.daia12@yahoo.com>
2016-07-11 16:13:24 +0300

    TabBarTest

---
+++ TabBarTest/BlueViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  TabBarTest
+//
+//  Created by Diana Daia on 7/11/16.
+//  Copyright © 2016 Diana. All rights reserved.
+//
+
+import UIKit
+
+class BlueViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .blueColor()
+		
+		print("blue")
+		
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
---
+++ TabBarTest/YellowViewController.swift
@@ -0,0 +1,30 @@
+//
+//  YellowViewController.swift
+//  TabBarTest
+//
+//  Created by Diana Daia on 7/11/16.
+//  Copyright © 2016 Diana. All rights reserved.
+//
+
+import UIKit
+
+class YellowViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		view.backgroundColor = .yellowColor()
+		
+		print("yellow")
+		
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+}
+
